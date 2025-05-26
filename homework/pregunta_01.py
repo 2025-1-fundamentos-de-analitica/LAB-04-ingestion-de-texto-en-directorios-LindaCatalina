deea83e44c92bd61186b4035959d769463079e5f
Linda Catalina Correa Lozano <licorrea@unal.edu.co>
2025-05-26 15:28:51 -0500

    Laboratorio 4

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,9 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
+import zipfile
+import os
+import pandas as pd
 
 def pregunta_01():
     """
@@ -71,3 +74,78 @@ def pregunta_01():
 
 
     """
+
+    ZIPFILE = "files/input.zip"
+    outdir = "input"
+
+    if not os.path.exists(ZIPFILE):
+        print(f"El archivo {ZIPFILE} no existe.")
+        return
+    with zipfile.ZipFile(ZIPFILE, 'r') as ZIPFILE_ref:
+        ZIPFILE_ref.extractall(outdir)
+        print(f"El archivo {ZIPFILE} descomprimido correctamente en {outdir}")
+
+
+pregunta_01()
+
+outdir = os.path.join('files', 'output')
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+phrases_test = []
+targets_test = []
+phrases_train = []
+targets_train = []
+
+testdir = os.path.join('input', 'input', 'test')
+traindir = os.path.join('input', 'input', 'train')
+
+sentdirs = ['positive', 'negative', 'neutral']
+
+for sent in sentdirs:
+    sent_path = os.path.join(testdir, sent)
+    
+    if not os.path.exists(sent_path):
+        print(f"El directorio {sent_path} no existe.")
+        continue
+
+
+    for filename in os.listdir(sent_path):
+        file_path = os.path.join(sent_path, filename)
+        
+        if os.path.isfile(file_path): 
+            with open(file_path, 'r', encoding='utf-8') as file:
+                phrase = file.read().strip()
+            phrases_test.append(phrase)
+            targets_test.append(sent)
+
+
+
+for sent in sentdirs:
+    sent_path = os.path.join(traindir, sent)
+    
+    if not os.path.exists(sent_path):
+        print(f"El directorio {sent_path} no existe.")
+        continue
+
+    for filename in os.listdir(sent_path):
+        file_path = os.path.join(sent_path, filename)
+        
+        if os.path.isfile(file_path):
+            with open(file_path, 'r', encoding='utf-8') as file:
+                phrase = file.read().strip()
+                
+            phrases_train.append(phrase)
+            targets_train.append(sent)
+
+
+test_data = pd.DataFrame({'phrase': phrases_test, 'target': targets_test})
+train_data = pd.DataFrame({'phrase': phrases_train, 'target': targets_train})
+
+test_data = test_data.sample(frac=1, random_state=42).reset_index(drop=True)
+train_data = train_data.sample(frac=1, random_state=42).reset_index(drop=True)
+
+test_data.to_csv(os.path.join(outdir, "test_dataset.csv"), index=False)
+train_data.to_csv(os.path.join(outdir, "train_dataset.csv"), index=False)
+
+print("Archivos 'train_dataset.csv' y 'test_dataset.csv' generados correctamente en la carpeta 'files/output'.")
